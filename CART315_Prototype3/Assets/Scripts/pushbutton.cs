b9dbc51a4afb0c73fb812084be21d2e0df81e3da
M33-Melissa <melissa_lim_17@hotmail.com>
2020-02-19 06:51:53 -0500

    Prototype 3 Basic Scene

---
+++ CART315_Prototype3/Assets/Scripts/pushbutton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pushbutton : MonoBehaviour
+{
+    Vector3 position;
+    void Start()
+    {
+
+    }
+
+    private void OnMouseEnter()
+    {
+        position = this.transform.position;
+        position.x = -1.16f;
+    }
+}
